669966237b5209b32a610cf8251a46999bf39168
Avinash Vallabhaneni <avinash.vllbh@gmail.com>
2014-10-14 22:37:07 -0700

    added custom instance method to return board_size

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,10 @@
 class Game < ActiveRecord::Base
+  GRID_SIZE = 4
+  # Serializing board and flipped attributes of game
+  serialize :board
+  serialize :flipped
+
+  def board_size
+    GRID_SIZE * GRID_SIZE
+  end
 end
