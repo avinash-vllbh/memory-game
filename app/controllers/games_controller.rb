6131d5a2d48a27f8b243e134206ce34c7f83f773
Avinash Vallabhaneni <avinash.vllbh@gmail.com>
2014-10-15 10:02:48 -0700

    added home page showing layout for cards on board

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,19 +3,22 @@ class GamesController < ApplicationController
     cards_collection = []
     @game = Game.new
     board_size = @game.board_size
+    grid_size = Math.sqrt(@game.board_size)
     cards = Card.pluck(:content) # Return only content column of cards table.
     # If board size is 16 (no of cards on board = 16) => Total pairs on board 8
     @game.total_pairs = board_size/2
     # Each element being added to the board should be added in pairs
+    # pick one card randomly from all the cards available
+    # Each card is appeneded to the array twice
     (@game.total_pairs).times do
-      card = cards.sample # pick one card randomly from all the cards available
-      2.times { cards_collection << card } # Each card is appeneded to the array twice
+      card = cards.sample
+      2.times { cards_collection << card }
     end
     cards_collection.shuffle! # Randomize the collection in place as the board need to be randomized for each new game
     # create a new board
     board = []
     cards_collection.each_with_index do |card,index|
-      board.push({"id" => index + 1, "card" => card})
+      board.push({"id" => index, "card" => card})
     end
     @game.board = board
     @game.save!
@@ -27,5 +30,6 @@ class GamesController < ApplicationController
   end
 
   def update
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root 'games#index'
 
-  resources :games
+  resources :games, :only => [:index, :show, :update]
   resources :cards
 
   # The priority is based upon order of creation: first created -> highest priority.
