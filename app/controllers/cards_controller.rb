6abbf8e31bb3300eb3e3f747050afc3bc1868c55
Avinash Vallabhaneni <avinash.vllbh@gmail.com>
2014-10-15 19:57:14 -0700

    added crud functionality to cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,22 +1,46 @@
 class CardsController < ApplicationController
+  before_filter :check_if_admin
   def index
+    @cards = Card.all
   end
 
   def new
+    @card = Card.new
   end
 
   def create
-  end
-
-  def show
-  end
-
-  def update
+    @card = Card.new(card_params)
+    if @card.save
+      flash[:info] = "New card is successfully created"
+      redirect_to cards_path
+    else
+      # flash[:alert] = "failed to create new card"
+      render "new"
+    end
   end
 
   def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    if @card.update(card_params)
+      flash[:info] = "Card has been successfully updated"
+      redirect_to cards_path
+    else
+      # flash[:alert] = "failed to update the card"
+      render "edit"
+    end
   end
 
   def destroy
+    Card.destroy(params[:id])
+    redirect_to cards_path
   end
+
+  private
+    def card_params
+      params.require(:card).permit(:content)
+    end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,3 @@
 class Card < ActiveRecord::Base
+  validates :content, presence: true, uniqueness: true, length: { in: 4..10 }
 end
