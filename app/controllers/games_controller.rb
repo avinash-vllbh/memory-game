5ab27228be6c5107dd7ae4850947bab0d50d9118
Avinash Vallabhaneni <avallabhaneni@customink.com>
2015-02-20 07:35:04 -0500

    index and show pages to use up partial in games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,48 +1,40 @@
 class GamesController < ApplicationController
+
   def index
-    cards_collection = []
-    @game = Game.new
-    board_size = @game.board_size
-    grid_size = Math.sqrt(@game.board_size)
-    cards = Card.pluck(:content) # Return only content column of cards table.
-    # Handle the edge case if card table is not seeded
-    if cards.size < 2
-      cards = Card.default_cards
-    end
-    # If board size is 16 (no of cards on board = 16) => Total pairs on board 8
-    @game.total_pairs = board_size/2
-    # Each element being added to the board should be added in pairs
-    # pick one card randomly from all the cards available
-    # Each card is appeneded to the array twice
-    (@game.total_pairs).times do
-      card = cards.sample
-      2.times { cards_collection << card }
-    end
-    cards_collection.shuffle! # Randomize the collection in place as the board need to be randomized for each new game
-    # create a new board
-    board = []
-    cards_collection.each_with_index do |card,index|
-      board.push({"id" => index, "card" => card})
-    end
-    @game.board = board
-    @game.save!
-    @hostname = request.host
+    @games = current_user.games
   end
 
+  def new
+    @game = Game.create
+    ###
+    # Saving the user_id association in controller
+    # Model are not supposed to have the state information of current_user
+    @game.user_id = current_user.id
+    @game.save
+    # @game_board = @game.game_board
+    # Refreshing when on new game view would cause to create a new game
+    redirect_to @game
+  end
+
+  def create
+
+  end
+  
   def show
     @game = Game.find(params[:id])
-    @hostname = request.host
+    @game_board = @game.game_board
   end
 
+  ###
+  # Update game preferences
   def update
     @game = Game.find(params[:id])
-    @game.flipped = params[:progress]
-    @game.pairs_found = params[:progress].size/2
-    if @game.save! # Bang to make sure it fails if an exception is found
-      render json: nil, status: 200
+    if @game.update_preferences(game_params)
+      flash[:notice] = "Your game preferences have been successfully updated!"
     else
-      render :json => {:errors => @contact.errors.messages}, :status => 422
+      flash[:alert] = "Your game preferences have not been updated!"
     end
+    redirect_to @game
   end
 
   def notify_user
@@ -51,5 +43,11 @@ class GamesController < ApplicationController
     else
       render json: nil, status: 422
     end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:id, :grid_size, :difficulty)
   end
 end
