8cf8cd8ce14f64ebdf315a630625e75beb0e5dbc
Avinash Vallabhaneni <avallabhaneni@customink.com>
2015-02-20 12:57:38 -0500

    model tests for cards to validate and verify business logic

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 describe Card do
-  before do
-    @card = Card.new
-  end
 
   describe "attribute validations" do
+    before do
+      @card = Card.new
+    end
     
     it "invalid without content" do
-      assert !@card.valid?
+      assert_not @card.valid?
     end
 
     it "invalid without minimum content" do
@@ -17,13 +17,36 @@ describe Card do
     end
 
   end
+
   describe ".random_cards_for_game" do
     
-    it "returns empty array when 0 cards in DB" do
+    it "returns a sample of two cards for an easy game" do
       cards = Card.random_cards_for_game("easy")
-      assert cards.empty?
+      assert_equal 2,cards.size
     end
-    
+
+    it "returns a sample of four cards for a medium game" do
+      cards = Card.random_cards_for_game("medium")
+      assert_equal 4, cards.size
+    end
+
+    it "returns empty array when there are no cards in DB" do
+      Card.destroy_all
+      cards = Card.random_cards_for_game("easy")
+      assert_equal 0, cards.size
+    end
+
+  end
+
+  describe ".cards_collection" do
+    before do
+      @game = games(:easy_game)
+    end
+
+    it "returns a collection of cards needed for the game" do
+      cards = Card.cards_collection(@game)
+      assert_equal @game.total_pairs * 2, cards.size
+    end
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 # require "test/unit"
-# require 'mocha/mini_test'
+require 'mocha/mini_test'
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
